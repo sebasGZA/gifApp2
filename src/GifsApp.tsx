ce72439568287d0f5e56901b6d777bee824b06e9
sebasGZA <sebassga34@gmail.com>
2026-03-31 23:11:29 -0500

    feat: adapt the gifts response in component

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react"
 
-import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { SearchBar } from "./shared/components/SearchBar"
 import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
+import type { Gif } from "./gifs/interfaces/gif.interface"
 
 
 export const GifsApp = () => {
 
-    const [previousTerms, setPreviousTerms] = useState(['']);
+    const [gifs, setGifs] = useState<Gif[]>([]);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
     const handleTermClicked = (term: string) => {
         console.log(term)
     }
@@ -24,7 +26,7 @@ export const GifsApp = () => {
         setPreviousTerms(currentTerms);
 
         const gifs = await getGifsByQuery(query);
-        console.log({ gifs })
+        setGifs(gifs);
     }
 
     return (
@@ -44,7 +46,7 @@ export const GifsApp = () => {
                 onLabelClick={handleTermClicked}
             />
 
-            <GifList gifs={mockGifs} />
+            <GifList gifs={gifs} />
         </>
     )
 }
