b4b41e824c085f3654e5ff91918b8a828f31b850
sebasGZA <sebassga34@gmail.com>
2026-03-31 21:23:12 -0500

    feat: create some components

---
+++ src/gifs/components/PreviousSearches.tsx
@@ -0,0 +1,12 @@
+export const PreviousSearches = () => {
+    return (
+        <div className="previous-searches">
+            <h2>Previous search</h2>
+            <ul className="previous-searches-list">
+                <li>Goku</li>
+                <li>Saitama</li>
+                <li>Elder ring</li>
+            </ul>
+        </div>
+    )
+}
