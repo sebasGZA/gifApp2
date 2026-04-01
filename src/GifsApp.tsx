a070dd5b1ca5ee2bef6b554a7a48fefeef9803a1
sebasGZA <sebassga34@gmail.com>
2026-03-31 23:01:22 -0500

    feat: implement axios and async http requests

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -5,6 +5,7 @@ import { CustomHeader } from "./shared/components/CustomHeader"
 import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { SearchBar } from "./shared/components/SearchBar"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action"
 
 
 export const GifsApp = () => {
@@ -14,13 +15,16 @@ export const GifsApp = () => {
         console.log(term)
     }
 
-    const handleSearch = (query: string = '') => {
+    const handleSearch = async (query: string = '') => {
         if (!query.trim().toLocaleLowerCase()) return;
         if (previousTerms.includes(query)) return;
 
         const currentTerms = previousTerms.slice(0, 8);
         currentTerms.unshift(query);
         setPreviousTerms(currentTerms);
+
+        const gifs = await getGifsByQuery(query);
+        console.log({ gifs })
     }
 
     return (
