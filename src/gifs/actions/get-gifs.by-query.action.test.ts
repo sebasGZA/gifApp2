71f5cf609fef564a02ff0f7609022d41221083ff
sebasGZA <sebassga34@gmail.com>
2026-04-07 14:43:13 -0500

    feat: implement axios adapter mock 2

--- src/gifs/actions/get-gifs.by-query.action.test.ts
+++ src/gifs/actions/get-gifs.by-query.action.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { getGifsByQuery } from "./get-gifs-by-query.action";
 import AxiosMockAdapter from 'axios-mock-adapter'
 import { giphyApi } from "../api/giphy.api";
@@ -7,18 +7,6 @@ import { giphySearchResponseMock } from '../../../test/mocks/giphy.response.data
 
 describe('getGifsByQuery', () => {
     const mock = new AxiosMockAdapter(giphyApi);
-    // test('should ', async () => {
-    //     const gifs = await getGifsByQuery('sun')
-    //     const [gif1] = gifs;
-
-    //     expect(gif1).toStrictEqual({
-    //         id: expect.any(String),
-    //         height: expect.any(Number),
-    //         width: expect.any(Number),
-    //         title: expect.any(String),
-    //         url: expect.any(String),
-    //     })
-    // });
 
     test('should return a list of gifs', async () => {
         mock.onGet('/search').reply(200, giphySearchResponseMock);
@@ -34,5 +22,14 @@ describe('getGifsByQuery', () => {
         })
 
         expect(gifs.length).toBe(25);
+    })
+
+    test('should return an empty list of gifs if query is empty', async () => {
+        mock.restore()
+
+        mock.onGet('/search').reply(200, giphySearchResponseMock);
+        const gifs = await getGifsByQuery('');
+        expect(gifs.length).toBe(0)
+
     })
 })
