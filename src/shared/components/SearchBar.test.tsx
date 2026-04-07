5e3574bf0b8fb7a423ccb3eb61afcaee507a7e25
sebasGZA <sebassga34@gmail.com>
2026-04-07 16:23:27 -0500

    feat: search bar tests with effects and debounces 2

--- src/shared/components/SearchBar.test.tsx
+++ src/shared/components/SearchBar.test.tsx
@@ -25,4 +25,52 @@ describe('SearchBar', () => {
         })
     })
 
+    test('should call only once with the last value(debounce)', async () => {
+        const onQuery = vi.fn();
+        render(<SearchBar onQuery={onQuery} />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, {
+            target: {
+                value: 'test'
+            },
+        });
+        fireEvent.change(input, {
+            target: {
+                value: 't'
+            },
+        });
+
+        await waitFor(() => {
+            expect(onQuery).toHaveBeenCalledTimes(1);
+            expect(onQuery).toHaveBeenCalledWith('t')
+        })
+    })
+
+    test('should  call onQuery when buttom clicked with input value', () => {
+        const onQuery = vi.fn();
+        render(<SearchBar onQuery={onQuery} />);
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change(input, {
+            target: {
+                value: 'test'
+            },
+        });
+
+        const button = screen.getByRole('button');
+        fireEvent.click(button);
+
+        expect(onQuery).toHaveBeenCalled();
+    })
+
+    test('should the input has the coorect placeholder value', () => {
+        render(<SearchBar onQuery={() => { }} placeholder="Search gifs" />)
+
+        expect(screen.getByPlaceholderText('Search gifs')).toBeDefined()
+    })
+
+
+
+
 })
